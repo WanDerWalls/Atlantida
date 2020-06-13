d99d42bd64d9a01b2de7efb694c801733ebf0f6f
WanDerWalls <r.bloha.work@gmail.com>
2020-06-13 14:52:56 +0300

    first

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+	$('.big-slider__body').slick({
+		infinite: false,
+	});
+	$('.feedback__body').slick({
+		infinite: false,
+		arrows: false,
+		dots: true,
+	});
+});
